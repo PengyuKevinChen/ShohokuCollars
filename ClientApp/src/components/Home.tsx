3163872d8680a62cbe9391c3f788758bf1bb24e0
Pengyu Chen <pengyu.chen@carsales.com.au>
2020-08-18 23:04:56 +1000

    testing repo transfer

--- ClientApp/src/components/Home.tsx
+++ ClientApp/src/components/Home.tsx
@@ -4,7 +4,7 @@ import {connect} from 'react-redux';
 const Home = () => (
     <div>
         <h1>ShohokuCollars</h1>
-            <p>the single place to log your basketball games</p>
+        <p>the single place to log your basketball games</p>
         <h3>Book a game</h3>
         <h3>New collar sign up</h3>
         <h3>Log a game</h3>
